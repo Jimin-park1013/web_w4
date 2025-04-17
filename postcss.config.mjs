6b8287b761b898bf7b5d9ca0d4dcc2d12b4adeed
Jimin-park1013 <as65253211@gmail.com>
2025-04-17 22:55:48 +0800

    final01

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,11 +1,5 @@
 const config = {
   plugins: ["@tailwindcss/postcss"],
-};
-
-
-
-export default config;
-module.exports = {
   theme: {
     extend: {
       keyframes: {
@@ -19,4 +13,6 @@ module.exports = {
       },
     },
   },
-}
+};
+
+export default config;
